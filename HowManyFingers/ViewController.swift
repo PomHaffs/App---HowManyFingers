247eaf50aab36600f5710c8a373c51239fee7a53
PomHaffs <tomashaffenden@icloud.com>
2016-12-09 13:26:20 +1100

    added logic

--- HowManyFingers/ViewController.swift
+++ HowManyFingers/ViewController.swift
@@ -10,17 +10,36 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    @IBOutlet weak var guessedNumber: UITextField!
-    
-    @IBOutlet weak var responseText: UILabel!
-    
-    @IBAction func submitButtonPressed(_ sender: Any) {
+    @IBOutlet weak var fingerTextField: UITextField!
+   
+    @IBAction func guessButtonPressed(_ sender: Any) {
+        
+        let randomNum = Int(arc4random_uniform(11))
+        let guess = Int(fingerTextField.text!)
+        
+        //var output = resultLabel.text
+        
+        if guess == randomNum {
+            resultLabel.text = "Well Done! That is her fav number of fingers!"
+        } else if guess! <= randomNum {
+            resultLabel.text = "Nope, more fingers than that!"
+        } else if guess! >= randomNum {
+            resultLabel.text = "Nope, too many fingers"
+        } else if guess == 0 {
+            resultLabel.text = "Come on, you know she loves fingers"
+        } else {
+            resultLabel.text = "Something has gone wrong with the logic???"
+        }
+        
+        
     }
+    
+    @IBOutlet weak var resultLabel: UILabel!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+    
     }
 
     override func didReceiveMemoryWarning() {
---
+++ HowManyFingers/model.swift
@@ -0,0 +1,9 @@
+//
+//  model.swift
+//  HowManyFingers
+//
+//  Created by Tomas-William Haffenden on 9/12/16.
+//  Copyright © 2016 PomHaffs. All rights reserved.
+//
+
+import Foundation
